cadad25aa8a2aa60f39971bffb37000c1a88bea9
Lun-Dev <lunthedev@gmail.com>
2021-09-24 02:05:30 +0800

    Modified: index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const containerEl = document.querySelector("#container") // Get the container that we will create the grid within it
+const buttonEl = document.querySelector("#clearBtn")
 
 function loadGrid() {
     let drawing = "" 
@@ -16,3 +17,16 @@ for (let i = 0; i < boxEl.length; i++) {
         boxEl[i].classList.add("blackBox"); // When mouseover, add blackbox div
     });
 }
+
+for (let i = 0; i < boxEl.length; i++) {
+    buttonEl.addEventListener("click", function() {
+        boxEl[i].classList.remove("blackBox"); // When clicked, remove blackbox div
+    });
+}
+
+// for (let i = 0; i < boxEl.length; i++) {
+//     eraserBtm.addEventListener("mouseover", function() {
+//         boxEl[i].classList.remove("blackBox"); // When clicked, remove blackbox div
+//     });
+// }
+
