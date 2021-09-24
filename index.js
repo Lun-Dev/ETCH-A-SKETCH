1524b583b33dc9a09adf3ff5004eaa6bcdf7ddc7
Lun-Dev <lunthedev@gmail.com>
2021-09-25 03:12:30 +0800

    Added: Eraser, Reset, Color Picker function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const containerEl = document.querySelector("#container") // Get the container that we will create the grid within it
 const clearBtn = document.querySelector("#clearBtn")
-// const eraserBtn = document.querySelector("#eraserBtn")
+const eraserBtn = document.querySelector("#eraserBtn")
+const resetBtn = document.querySelector("#resetBtn")
 
 function loadGrid() {
     let drawing = "" 
@@ -10,17 +11,42 @@ function loadGrid() {
     containerEl.innerHTML = drawing // Manipulate the DOM in one go after we concatenated the for loop above
 }
 
+function mouseOver() {
+    for (let i = 0; i < boxEl.length; i++) {
+        boxEl[i].addEventListener("mouseover", function() {
+            boxEl[i].classList.add("blackBox"); // When mouseover, add blackbox div
+        });
+    };
+};
+    
+function clearBox() {
+    clearBtn.addEventListener("click", function() {
+        for (let i = 0; i < boxEl.length; i++) {
+            boxEl[i].classList.remove("blackBox");
+        }
+    });
+};
+
+function reset() {
+    resetBtn.addEventListener("click", function() {
+        mouseOver()
+    })
+};
+    
 loadGrid()
 const boxEl = document.querySelectorAll("#box") // Get the new box div
+mouseOver()
+clearBox()
+eraseBox()
+reset()
 
-for (let i = 0; i < boxEl.length; i++) {
-    boxEl[i].addEventListener("mouseover", function() {
-        boxEl[i].classList.add("blackBox"); // When mouseover, add blackbox div
+function eraseBox() {
+    eraserBtn.addEventListener("click", function() {
+        for (let i = 0; i < boxEl.length; i++) {
+            boxEl[i].addEventListener("mouseover", function() {
+                boxEl[i].classList.remove("blackBox"); 
+            })
+        }
     });
-}
+};
 
-clearBtn.addEventListener("click", function() {
-    for (let i = 0; i < boxEl.length; i++) {
-        boxEl[i].classList.remove("blackBox");
-    }
-})
